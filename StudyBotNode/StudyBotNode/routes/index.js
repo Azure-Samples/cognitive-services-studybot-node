b48bd029bbaa78225b752056af81fa005f61826b
Winona Azure <v-wiazur@microsoft.com>
2019-01-02 16:28:12 -0800

    Added the Node app

---
+++ StudyBotNode/StudyBotNode/routes/index.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+const express = require('express');
+const router = express.Router();
+
+// The website tabs.
+function openSite(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// GET home page.
+router.get('/', function (req, res) {
+    res.render('index', {
+        title: 'Express'
+    });
+});
+
+module.exports = router;
